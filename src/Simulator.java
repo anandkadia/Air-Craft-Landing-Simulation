9f0aa44920a425f44bbf03407e8e9421bc9e7121
anandkadia <anand.kadia@gmail.com>
2015-04-17 20:57:35 -0500

    errors
    ak

--- src/Simulator.java
+++ src/Simulator.java
@@ -13,49 +13,44 @@ class Simulator
 		//THE LOOP - 37 Times
 		for(int i=0; i<37; i++)
 		{
-			for(int j=0; j<12; j++)
-			{
-				int testCaseNumber = Integer.parseInt(testcases[i][0]);
-				
-				int expectedScenario = Integer.parseInt(testcases[i][2]);
-				
-				int speed = Integer.parseInt(testcases[i][3]);
-				
-				boolean gearDown = false;
-				if(testcases[i][4].equals("Y"))
-					gearDown = true;
-				
-				int altitude = Integer.parseInt(testcases[i][5]);
-				
-				int time = Integer.parseInt(testcases[i][6]);
-				
-				boolean gearNotDownAlarm = false;
-				if(testcases[i][7].equals("X"))
-					gearNotDownAlarm = true;
-				
-				boolean gearAirSpeedAlarm = false;
-				if(testcases[i][8].equals("X"))
-					gearAirSpeedAlarm = true;
-				
-				boolean airBrakeWarning = false;
-				if(testcases[i][9].equals("X"))
-					airBrakeWarning = true;
-				
-				boolean gearOverrideWarning = false;
-				if(testcases[i][10].equals("Y"))
-					gearOverrideWarning = true;
-				
-				boolean gearUpCommand = false;
-				if(testcases[i][11].equals("Y"))
-					gearUpCommand = true;
-				
-				computeSimulationState(testCaseNumber, expectedScenario, speed, gearDown, altitude, time, gearNotDownAlarm, gearAirSpeedAlarm, airBrakeWarning, gearOverrideWarning, gearUpCommand);
-				//System.out.print(testcases[i][j]+"\t");
-			}
-			System.out.println();
+			int testCaseNumber = Integer.parseInt(testcases[i][0]);
+			
+			int expectedScenario = Integer.parseInt(testcases[i][2]);
+			
+			int speed = Integer.parseInt(testcases[i][3]);
+			
+			boolean gearDown = false;
+			if(testcases[i][4].equals("Y"))
+				gearDown = true;
+			
+			int altitude = Integer.parseInt(testcases[i][5]);
+			
+			int time = Integer.parseInt(testcases[i][6]);
+			
+			boolean gearNotDownAlarm = false;
+			if(testcases[i][7].equals("X"))
+				gearNotDownAlarm = true;
+			
+			boolean gearAirSpeedAlarm = false;
+			if(testcases[i][8].equals("X"))
+				gearAirSpeedAlarm = true;
+			
+			boolean airBrakeWarning = false;
+			if(testcases[i][9].equals("X"))
+				airBrakeWarning = true;
+			
+			boolean gearOverrideWarning = false;
+			//if(testcases[i][10].equals("X"))
+			//	gearOverrideWarning = true;
+			
+			boolean gearUpCommand = false;
+			//if(testcases[i][11].equals("X"))
+				//gearUpCommand = true;
+			
+			computeSimulationState(testCaseNumber, expectedScenario, speed, gearDown, altitude, time, gearNotDownAlarm, gearAirSpeedAlarm, airBrakeWarning, gearOverrideWarning, gearUpCommand);
+			//System.out.print(testcases[i][j]+"\t");
 		}
-		
-		
+		System.out.println();
 	}
 
 
@@ -63,23 +58,13 @@ class Simulator
 			int expectedScenario, int speed, boolean gearDown, int altitude,
 			int time, boolean gearNotDownAlarm, boolean gearAirSpeedAlarm,
 			boolean airBrakeWarning, boolean gearOverrideWarning,
-			boolean gearUpCommand) {
-		
-		/*intializing all parameters for computeSimulaionState method
-		int testCaseNumber1 = testCaseNumber;
-		int expectedScenario = expectedScenario;
-		int speed = speed; 
-		boolean gearDown = gearDown;
-		int altitude = altitude;
-		int time = time;
-		boolean gearNotDownAlarm = gearNotDownAlarm; 
-		boolean gearAirSpeedAlarm = gearAirSpeedAlarm;
-		boolean airBrakeWarning = airBrakeWarning;
-		boolean gearOverrideWarning = gearOverrideWarning;
-		boolean gearUpCommand = gearUpCommand; */
+			boolean gearUpCommand) 
+	{
 		
 		// Conditions to decide display scenario
-		
+		System.out.println(testCaseNumber+" "+expectedScenario+" "+speed+" "+gearDown+" "
+							+altitude+" "+time+" "+gearNotDownAlarm+" "+gearAirSpeedAlarm+" "
+							+airBrakeWarning+" "+gearOverrideWarning+" "+gearUpCommand);
 		
 		
 	}
